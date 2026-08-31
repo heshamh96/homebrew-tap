c7b5d0e229cc724a6ef7750fd8425c03b2896ecf
Hesham <hesham.h.96@gmail.com>
2026-08-31 09:24:13 +0300

    setups 0.1.5

--- Casks/setups.rb
+++ Casks/setups.rb
@@ -1,8 +1,8 @@
 cask "setups" do
-  version "0.1.4"
+  version "0.1.5"
 
   on_arm do
-    sha256 "3b63d9fdf29dc8d2226cd9864bc0423ad51749a3e57999c554b5157d288fad44"
+    sha256 "3fca4a3c441dcc2d4d28eccd3909ba1dd3e199c5ff2baa99b08d9f6642d3aff6"
     url "https://github.com/meta-thinking/homebrew-tap/releases/download/setups-v#{version}/Setups_#{version}_aarch64.app.tar.gz"
   end
 
